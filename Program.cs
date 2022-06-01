7d3103537718c5434e238ed6dfa80b746fceb25d
Saikumar <muthyalasaikumar9@gmail.com>
2022-06-01 20:49:25 +0530

    commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegexProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n----------------WELCOME TO THE REGULAR EXPRESSION (REGEX) PROGRAM------------------------\n\n");
+        }
+    }
+}
